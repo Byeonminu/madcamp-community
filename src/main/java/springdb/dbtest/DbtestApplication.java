714f1416a8ac881f6c60635072a12e550898efa0
minwoo Byeon <97205235+Byeonminu@users.noreply.github.com>
2022-07-17 17:22:04 +0900

    [success] deploy (AWS)

--- src/main/java/springdb/dbtest/DbtestApplication.java
+++ src/main/java/springdb/dbtest/DbtestApplication.java
@@ -2,15 +2,16 @@ package springdb.dbtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DbtestApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
+//	@RequestMapping("/")
+//	String home() {
+//		return "Hello World!";
+//	}
 	public static void main(String[] args) {
 
 		SpringApplication.run(DbtestApplication.class, args);
