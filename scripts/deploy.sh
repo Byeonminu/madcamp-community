9b4a7bff5a9ace7bb5849928712dad91e9c9b4eb
minwoo Byeon <97205235+Byeonminu@users.noreply.github.com>
2022-07-25 02:02:05 +0900

    refactor: Deploy log

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,12 @@
-echo "> 현재 실행 중인 Docker 컨테이너 pid 확인!" >> /home/ubuntu/app/deploy.log
+PROJECT_ROOT="/home/ubuntu/app"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인!" >> $DEPLOY_LOG
 CURRENT_PID=$(sudo docker container ls -q)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ubuntu/app/deploy.log
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> $DEPLOY_LOG
 else
   echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
   sudo docker stop $CURRENT_PID
